49b5bb28bbf72a4c2c170479dd7ae8c20fbf52c3
camot0328 <camot0328@gmail.com>
2025-03-24 17:40:45 +0900

    상근이 했던부분까지

--- src/Product.java
+++ src/Product.java
@@ -1,21 +1,18 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-
 public class Product {
   String productName;
   int price;
   String productNo;
   String information;
+  int quantity;
   String productAdd;
   String productUpdate;
 
-  public Product(String productName, int price, String information, String productNo) {
+  public Product(String productName, int price, String information, String productNo, int quantity) {
     this.productName = productName;
     this.price = price;
     this.information = information;
     this.productNo = productNo;
+    this.quantity = quantity;
 //    this.productAdd = productAdd;
 //    this.productUpdate = productUpdate;
   }
